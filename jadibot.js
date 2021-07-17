90f8447e50f0f98c03542208e2fc79803b1b899f
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2021-07-17 07:11:58 +0700

    update jadibot.js

--- jadibot.js
+++ jadibot.js
@@ -73,10 +73,6 @@ alasan = '-'
 //=================================================//
 module.exports = conn = async (conn, mek) => {
 	try {
-        if (!mek.hasNewMessage) return
-        mek = mek.messages.all()[0]
-		if (!mek.message) return
-		if (mek.key && mek.key.remoteJid == 'status@broadcast') return
 		global.blocked
         	mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
         	const content = JSON.stringify(mek.message)
